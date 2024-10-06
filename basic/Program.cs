0b05a05384c83d788dab6eee6691787a44a6c881
HongEngVathana <engvathanahong@gmail.com>
2024-10-06 23:29:05 +0700

    hello

---
+++ basic/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace basic;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int code = 123;
+        double salary = 20000.99;
+        char gender= 'M';
+        string name = "Hong EngVathana";
+        bool isSingle = true;
+        Console.WriteLine("Hello All My Nameis :Hong EngVathana,I Live in Sream Reap(Angkor Wat) in Cambodia, So I'm a Poor Student I Live With Small Family And I Love My Family So Muose, Today I'm a Student in Rupp Year 2, My Mager is IT, I Have a plean For My lift to learn in USA  I want to be the base Developer ");
+        Console.WriteLine("Today 6 Oct 2024 I learn C# Becouse I want to be a Developer FullStack With C# .NET ");
+        Console.WriteLine(code + salary + gender + name + isSingle);
+        Console.WriteLine("=========================================");
+        Console.WriteLine("Code    :" + code);
+        Console.WriteLine("Salary  :" + salary+"$");
+        Console.WriteLine("Name    :" + name);
+        Console.WriteLine("isSingle:" + isSingle);
+        Console.WriteLine("=========================================");
+    }
+}
+
